75314812abf9c15eaae1189b13efd9cd36d83627
Mohsen Galal <devmohsengalal@gmail.com>
2020-04-07 21:53:53 +0200

    side nav fix

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,20 @@
 $(document).ready(function(){
+  // slider Toggle
   $("#slide-toggle").click(function(){
-    console.log($(this));
-    
     $(this).toggleClass("reversed");
     $(".side-nav").toggleClass("closed");
     $(".main-container").toggleClass("full-container")
+  });
+// Slide Collapse
+  $('.list-group-item[data-toggle="collapse"]').click(function() {
+    if($(this).hasClass("collapsed"))
+   {
+    $(this).find(".expand-icon").attr("src", "assets/images/minus-icon.png")
+    $(this).find(".expand-icon").css({"top":"26px"})
+   }
+   else{
+    $(this).find(".expand-icon").attr("src", "assets/images/plus-icon.png")
+    $(this).find(".expand-icon").css({"top":"20px"})
+   }
   })
 })
