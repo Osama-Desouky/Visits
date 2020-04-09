e777f4a61d008ce931ff946fcb392d699e15ef55
Mohsen Galal <devmohsengalal@gmail.com>
2020-04-09 02:22:34 +0200

    cards and collapse sidebar

--- js/custom.js
+++ js/custom.js
@@ -17,4 +17,40 @@ $(document).ready(function(){
     $(this).find(".expand-icon").css({"top":"20px"})
    }
   })
+  var cardBtn = $(".card-btn");
+  $(".card-btn").click(function(){
+    menuBoxHide(cardBtn);
+    cardBtn = $(this)
+    menuBoxShow(cardBtn);
+  })
+  $(".close-panel").click(function(){    
+    menuBoxHide(cardBtn)
+  })
+  $(".mini-panel").click(function(){    
+    minimizeCard(cardBtn)
+  })
+  $(".max-panel").click(function(){    
+    maxmizeCard(cardBtn)
+  })
+  function menuBoxHide(element)
+  {
+    var menuBox= element.parent().find("ul");
+    menuBox.hide();
+  }
+  function menuBoxShow(element)
+  {
+    var menuBox= element.parent().find("ul");
+    menuBox.show();
+  }
+  function minimizeCard(element){
+    var cardBody=element.parents(".sm-card").find(".card-body");
+    menuBoxHide(element)
+    cardBody.collapse("hide");
+    
+  }
+  function maxmizeCard(element){
+    var cardBody=element.parents(".sm-card").find(".card-body");
+    menuBoxHide(element)
+    cardBody.collapse("show");
+  }
 })
