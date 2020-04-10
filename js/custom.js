ab67480969ca516be4483d0347275e79cfdd398f
Mohsen Galal <devmohsengalal@gmail.com>
2020-04-10 07:58:04 +0200

    dashboard final

--- js/custom.js
+++ js/custom.js
@@ -1,59 +1,87 @@
-$(document).ready(function(){
+$(document).ready(function () {
   // slider Toggle
-  $("#slide-toggle").click(function(){
+  $("#slide-toggle").click(function () {
     $(this).toggleClass("reversed");
     $(".side-nav").toggleClass("closed");
-    $(".main-container").toggleClass("full-container")
+    $(".main-container").toggleClass("full-container");
   });
-  $(".pop-details").click(function(){
-    $(this).parent().find(".details-card").toggle()
-  })
-// Slide Collapse
-  $('.list-group-item[data-toggle="collapse"]').click(function() {
-    if($(this).hasClass("collapsed"))
-   {
-    $(this).find(".expand-icon").attr("src", "assets/images/minus-icon.png")
-    $(this).find(".expand-icon").css({"top":"26px"})
-   }
-   else{
-    $(this).find(".expand-icon").attr("src", "assets/images/plus-icon.png")
-    $(this).find(".expand-icon").css({"top":"20px"})
-   }
-  })
+  $(".pop-details").click(function () {
+    $(this).parent().find(".details-card").toggle();
+  });
+  // Slide Collapse
+  $('.list-group-item[data-toggle="collapse"]').click(function () {
+    if ($(this).hasClass("collapsed")) {
+      $(this).find(".expand-icon").attr("src", "assets/images/minus-icon.png");
+      $(this).find(".expand-icon").css({ top: "26px" });
+    } else {
+      $(this).find(".expand-icon").attr("src", "assets/images/plus-icon.png");
+      $(this).find(".expand-icon").css({ top: "20px" });
+    }
+  });
   var cardBtn = $(".card-btn");
-  $(".card-btn").click(function(){
+  $(".card-btn").click(function () {
     menuBoxHide(cardBtn);
-    cardBtn = $(this)
+    cardBtn = $(this);
     menuBoxShow(cardBtn);
-  })
-  $(".close-panel").click(function(){    
-    menuBoxHide(cardBtn)
-  })
-  $(".mini-panel").click(function(){    
-    minimizeCard(cardBtn)
-  })
-  $(".max-panel").click(function(){
-    maxmizeCard(cardBtn)
-  })
-  function menuBoxHide(element)
-  {
-    var menuBox= element.parent().find("ul");
+  });
+  $(".close-panel").click(function () {
+    menuBoxHide(cardBtn);
+  });
+  $(".mini-panel").click(function () {
+    minimizeCard(cardBtn);
+  });
+  $(".max-panel").click(function () {
+    maxmizeCard(cardBtn);
+  });
+  $(".delete-panel").click(function (e) {
+    if (e.currentTarget == e.target) {
+      deleteNotification(cardBtn);
+    }
+  });
+  $(".read-panel").click(function (e) {
+    if (e.currentTarget == e.target) {
+      readToggle(cardBtn, false);
+    }
+  });
+  $(".unread").click(function (e) {
+    if (e.currentTarget == e.target) {
+      readToggle(cardBtn, true);
+    }
+  });
+  function menuBoxHide(element) {
+    var menuBox = element.parent().find("ul");
     menuBox.hide();
   }
-  function menuBoxShow(element)
-  {
-    var menuBox= element.parent().find("ul");
+  function menuBoxShow(element) {
+    var menuBox = element.parent().find("ul");
     menuBox.show();
   }
-  function minimizeCard(element){
-    var cardBody=element.parents(".sm-card , .regular-card").find(".card-body");
-    menuBoxHide(element)
+  function minimizeCard(element) {
+    var cardBody = element
+      .parents(".sm-card , .regular-card")
+      .find(".card-body");
+    menuBoxHide(element);
     cardBody.collapse("hide");
-    
   }
-  function maxmizeCard(element){
-    var cardBody=element.parents(".sm-card , .regular-card").find(".card-body");
-    menuBoxHide(element)
+  function maxmizeCard(element) {
+    var cardBody = element
+      .parents(".sm-card , .regular-card")
+      .find(".card-body");
+    menuBoxHide(element);
     cardBody.collapse("show");
   }
-})
+  function readToggle(element, read) {
+    var noificationCard = element.parents(".notification-card");
+    menuBoxHide(element);
+    if (read) {
+      noificationCard.addClass("unread");
+    } else {
+      noificationCard.removeClass("unread");
+    }
+  }
+  function deleteNotification(element) {
+    var noificationCard = element.parents(".notification-card");
+    menuBoxHide(element);
+    noificationCard.remove();
+  }
+});
